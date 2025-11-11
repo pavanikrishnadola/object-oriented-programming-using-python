25f5cc4eeeb7fc2a9cedcd0a54d944655aca411c
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-11-11 12:25:23 +0530

    Add files via upload

---
+++ textfiles/mymodules/greet.py
@@ -0,0 +1,7 @@
+def greetone():
+    print("welcome to python")
+
+def greettwo():
+    """This prints a msg when called"""
+    print("how are you today")
+
---
+++ textfiles/mymodules/test.py
@@ -0,0 +1,8 @@
+import greets
+greets.greet()
+
+from greets import *
+greet()
+
+from greets import greet
+greet()
