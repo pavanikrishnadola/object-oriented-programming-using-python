ed1c8495c8497b521adbc09305c6ed7a73d94836
Pavani Krishna Dola <pavanikrishnadola01@gmail.com>
2025-11-13 11:15:08 +0530

    Add files via upload

---
+++ tkintercode/tkinter02.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root=Tk()
+root.title("Demo Application")
+root.geometry("300x300+400+200")
+root.config(bg="lightgreen")
+root.resizable(0,0)
+mainloop()
---
+++ tkintercode/tkinter03.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root=Tk()
+root.title("Demo Application")
+root.geometry("300x300+400+200")
+root.config(bg="lightgreen")
+root.resizable(0,0)
+lb1=Label(text="Tkinter Application")
+lb1.pack()
+mainloop()
